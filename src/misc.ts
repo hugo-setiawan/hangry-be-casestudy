ef32a08c1b802b8b20685e5442dec236fe52cb9f
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2024-08-12 23:10:16 +0700

    feat: json response helper function
    Avoids code duplication regarding giving out JSON response to the incoming requests

--- src/misc.ts
+++ src/misc.ts
@@ -1,11 +1,24 @@
 import http from 'http';
-import { getReasonPhrase, type StatusCodes } from "http-status-codes";
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
 
 export const respondWithError = (errorCode: StatusCodes, message: string, req: http.IncomingMessage, res: http.ServerResponse) => {
     res.statusCode = errorCode;
 
+    res.setHeader("Content-Type", "application/json");
+
     res.end(JSON.stringify({
         status: getReasonPhrase(errorCode),
         description: message,
+    }));
+}
+
+export const respondWithJson = (data: Object, req: http.IncomingMessage, res: http.ServerResponse) => {
+    res.statusCode = StatusCodes.OK;
+
+    res.setHeader("Content-Type", "application/json");
+
+    res.end(JSON.stringify({
+        status: "OK",
+        data
     }));
 }
